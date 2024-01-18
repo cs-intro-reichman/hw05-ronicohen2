99ebae6b5e672ab316d72a6c6a1bc19f030f6bea
ronicohen2 <154340689+ronicohen2@users.noreply.github.com>
2024-01-18 19:16:00 +0200

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -15,10 +15,10 @@ public class GameOfLife
 		String fileName = args[0];
 		//// Uncomment the test that you want to execute, and re-compile.
 		//// (Run one test at a time).
-		 ///test1(fileName);
-		///test2(fileName);
+		 test1(fileName);
+		test2(fileName);
 		test3(fileName, 3);
-		/// play(fileName);
+		play(fileName);
 	}
 	
 	// Reads the data file and prints the initial board.
